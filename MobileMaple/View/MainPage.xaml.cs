9657f5da61a60d5f87edd9342997c998a664138e
Nelson Lamprecht <Sorbitol77@outlook.com>
2022-09-25 15:18:11 -0500

    initial copy

---
+++ MobileMaple/View/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace MobileMaple.View
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationPage.SetBackButtonTitle(this, "Back");
+        }
+
+        void BtnLedClicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new LedControllerPage());
+        }
+
+        void BtnServoClicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ServoControllerPage());
+        }
+
+        void BtnTemperatureClicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new TemperatureControllerPage());
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # monsterbox_remote
+
+# documentation 
+
+Based on: https://github.com/WildernessLabs/Meadow.Project.Samples/tree/main/Source/Hackster/Maple/MobileMaple
